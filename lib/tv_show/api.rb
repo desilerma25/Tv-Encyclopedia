3b12158a102b6bc2dcc063ba22ee2db2182142ff
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-10 20:59:58 -0600

    Updated API

--- lib/tv_show/api.rb
+++ lib/tv_show/api.rb
@@ -7,13 +7,16 @@ class API
         hash = JSON.parse(response)
         array_of_info = hash["info"]
         # binding.pry 
-        array_of_info.each do |info|
+        array_of_info.each do |info_hash|
+            info_instance = Info.new(name: info_hash["name"])
+            info_instance.summary = info_hash["summary"]
+            info_instance.rating = info_hash["rating"]["average"]
+            info_instance.genres = info_hash["genres"]
         end
     end
 
-    def create_info
-        self.fetch_info.each{|info| Info.new(info["name"], info["status"], info["rating"], info["genres"], info["summary"], info["embedded"]["cast"], info["embedded"]["seasons"], info["embedded"]["episodes"])}
-    end
+   
+   
 
 
 end
