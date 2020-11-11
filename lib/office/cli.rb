331930db65bf5c7e870bba3b1d9b69ee17d8cc8d
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-10 21:29:54 -0600

    Updated CLI to make choices and create methods for user.

--- lib/office/cli.rb
+++ lib/office/cli.rb
@@ -1,75 +1,54 @@
 require 'pry'
 class CLI
-
-
     def start
-        puts "Welcome! Want to know more about The Office (U.S)? Let me help!"
-        puts "Please select what information you would like to see."
-        self.display_shows
-        index = self.initial_input
-        query = CLI.shows[index]
-        api = API.new(query)
-        api.create_shows
-        Tv_Show.display_shows
-        index = self.secondary_input
-
-        Tv_Show.display_shows(index)
-        self.more_info?
-        self.another_show?
+        puts "Welcome!"
+        API.fetch_info
+        self.menu
     end
 
-    def input_to_index(input)
-        binding.pry
-        input.to_i-1
-    
-    end
+    def menu
+        puts "Would you like to learn more about The Office (U.S)?"
+        puts "Type 'yes' to proceed or select another key to exit."
+        if user_input == "yes" || "y"
+            puts "Great! What would you like to know about?"
+            display_options
+            ask_user_what_info
+            sleep(1)
+            menu
+        elsif user_input == search
 
-    # def more_info?
-    #     puts "Would you like to look at more details about this show?"
-    #     puts "1. Yes"
-    #     puts "2. No"
-    #     input = gets.chomp
-    #     puts "Would you like information on the cast, seasons, or episodes?"
-    #     display_more_options # create this method, 
-    #     input = gets.chomp
-    #     input.each do |option|
-
-
-    # end
-
-    # def another_show?
-    #     puts "Would you like to view information on another popular show?"
-    #     puts "1. Yes"
-    #     puts "2. No"
-    #     input = gets.chomp
-    #     index = input_to_index(input)
-    # end
-
-    def initial_input
-        input = gets.chomp
-        index = input_to_index(input)
-
-        if !index.between?(0,4)
-            puts "Please select a valid option from the menu."
-            self.initial_input
+            menu
         else
-            index
+            puts "Hope you enjoyed some nuggest of information!"
         end
     end
 
-    # def secondary_input
-    #     input = gets.chomp
-    #     index =
-    # end
-
-
-    def self.shows
-        @@shows
+    def ask_user_what_info
+        index = gets.chomp.to_i-1
+        max_limit = Office.all.length-1
+        until index.between?(0,max_limit)
+            puts "Sorry I don't understand what you were trying to select."
+            index = gets.chomp.to_i-1
+        end
+        info_instance = Office.all[index]
+        display_info_details(info_instance)
     end
 
-    def display_shows
-        CLI.shows.each_with_index{|show, index| puts "#{index+1}. #{show.capitalize}"}
+    def display_info_details(info)
+        sleep(1)
+        puts "\nRating:" + info.rating
+        puts "\nSummary:" + info.summary 
+        puts "\nGenres:" + info.genres 
     end
+
+    def display_list_of_options
+        Office.all.each.with_index(1) do |info, index|
+            puts "#{index}. #{info.rating}"
+            puts "#{index}. #{info.summary}"
+            puts "#{index}. #{info.genres}"
+    end
+
+
 end
 
 puts "CLI"
