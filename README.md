43f5ab56972aed7c21ee3b3d75f9ab9bbcb48995
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-15 13:22:47 -0600

    Created README.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Tv-Encyclopedia
-The application will allow you to get a deeper look into some of America's top television shows.
+Welcome all to the Tv Encyclopedia!!
+If you have had some interest or would like to know more regarding popular television shows you've come to the right place.
+
+Description:
+The Tv Encyclopedia allows a user to access a list of popular television shows. From the list, the user may select one to view further information on the show. After viewing the information, the user is given the option to save the tv show to their Favorites list so that they may return to these shows easier. 
+
+To Run The Program:
+In your terminal, prior to the start of the program, you must do a bundle install. To do so, simply enter 'bundle install' into your terminal. This will require and install and necessary gems to have the program run smoothly. Then you may continue, enter 'ruby bin/start' to get the program up and going.
+
+
+All data was generated using the TVMaze API: http://www.tvmaze.com/api
+
+Contributors:
+If you would like to add or tinker with this program please email me at desi.lerma96@yahoo.com to let me know.
 
--- lib/Show/cli.rb
+++ lib/Show/cli.rb
@@ -16,22 +16,23 @@ puts "|  -ooo--------------  :|||: |"
 puts "+----------------------------+"
 puts "  []                    []"
 
-    @@shows = ["I Love Lucy", "The Twilight Zone", "Schitt's Creek", "Breaking Bad", "Game of Thrones"]
-
+    @@shows = ["I Love Lucy", "The Twilight Zone", "Schitt's Creek", "Breaking Bad", "Game of Thrones", "The Voice", "Criminal Minds", "How I Met Your Mother", "The Walking Dead", "Sons of Anarchy", "The Colbert Report", "The Biggest Loser"]
+    @@favorite_shows = []
 
     def start
         puts "Welcome to the TV Encyclopedia!"
         sleep(1)
-        puts "Would you like to view your television show options? Enter 'yes' to continue or 'no' to exit."
+        puts "Would you like to view your television show options? Enter 'yes' to continue, 'no' to exit or 'view' to see your saved favorites."
         user_input = gets.strip.downcase
         if user_input == "yes" || user_input == 'y'
         puts "Please choose a popular television show from the list that you'd like information on."
         self.display_shows
         ask_for_user_second_input
         self.back_to_menu?
-        elsif
-            user_input == "no" || user_input == "n"
+        elsif user_input == "no" || user_input == "n"
             puts "Thanks for channel surfing!"
+        elsif user_input == "view"
+            view_favorites
         else
             puts "Hmmm, that wasn't right. Why don't we take it from the top?"
             sleep(2)
@@ -39,17 +40,49 @@ puts "  []                    []"
         end
     end
 
+    def view_favorites
+        if @@favorite_shows.empty?
+            puts "It appears you have not saved any shows yet."
+            sleep(1)
+            back_to_menu?
+        else
+            puts "Here are your favorite shows!"
+            puts "#{@@favorite_shows.join(", ")}"
+            sleep(1)
+            back_to_menu?
+        end
+    end
+
 
     def ask_for_user_second_input
         index = self.initial_input
-        if index.between?(0,4) 
+        if index.between?(0,11) 
             query = CLI.shows[index]
             api = API.new(query)
             x = api.create_shows
             display_show_details(x)
+            sleep(1)
+            save_favorite(x)
         else
-            puts "Please select a valid number from the list (1-5)."
+            puts "We interrupt this program because that was not a valid option. Please select a valid number from the list (1-12)."
             self.ask_for_user_second_input
+        end
+    end
+
+    def save_favorite(x)
+        puts "Would you like to add this show to your favorites?"
+        puts "Type 'yes' to save, 'no' to continue or 'exit' to leave." 
+        input = gets.strip.downcase
+        if input == "yes" || input == "y"
+            @@favorite_shows << x.name.to_s
+            puts "This show is now in your Favorites"
+        elsif input == "no" || input == "n"
+            puts "That's ok!! Maybe you'll find another show you're interested in."
+        elsif input == "exit"
+            puts "Thanks for channel surfing!"
+        else
+            puts "We interrupt this program because that was not a valid option. Please try again."
+            input = gets.strip.downcase
         end
     end
 
